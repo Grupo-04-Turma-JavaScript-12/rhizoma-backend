a583132235c08fe8b21ffcd3c9615112dad918fa
Giovanna Roberta <giiroberta18@gmail.com>
2026-01-16 12:20:34 -0300

    :sparkles: feat: Conexão no banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ColaboradorModule } from './colaborador/colaborador.module';
+import { Colaborador } from './colaborador/entities/colaborador.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_rhizoma',
+      entities: [Colaborador],
+      synchronize: true,
+      logging: true,
+    }),
+    ColaboradorModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/colaborador/services/colaborador.service.ts
+++ src/colaborador/services/colaborador.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, ILike, Repository } from 'typeorm';
